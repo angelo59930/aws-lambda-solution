afa81b8f86c9331a75fb4cb4d9cd6c96a64fe41d
Angelo Manfredi <manfrediangelosantiago10@gmail.com>
2024-05-01 00:36:59 -0300

    se modifica la lambda_handler para validar el funcionamiento del pipeline

--- app/api_publico.py
+++ app/api_publico.py
@@ -4,6 +4,6 @@ import json
 def lambda_handler(event, context):
     response = {
         "statusCode": 200,
-        "body": json.dumps("¡Hola desde AWS Lambda! Mi nombre es Gabriel"),
+        "body": json.dumps("¡Hola desde AWS Lambda! Mi nombre es ..."),
     }
     return response
